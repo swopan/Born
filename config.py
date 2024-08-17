88befab74fe1af604ed9226bb6d8ca85cebe48c3
swopan <38856499+swopan@users.noreply.github.com>
2024-08-17 09:30:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6314089978"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = ""
+DB_URI = "mongodb+srv://sunitverma080:eh0fYGutTA72cGrs@blaze.zwyit.mongodb.net/?retryWrites=true&w=majority&appName=Blaze"
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "kingurl.com")
